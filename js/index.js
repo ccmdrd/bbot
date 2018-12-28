e76f5c636a201628ae21df8f523f490be9cc960b
ESDC <esdc@ppp235.mctoronto.com>
2018-12-28 10:00:53 -0500

    refactor: test form

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-// Scripts for Boozebot
+// // Scripts for Boozebot
+// jQuery.ajax({
+//   url: 'https://lcboapi.com/stores',
+//   headers: { 'Authorization': 'Token ACCESS_KEY' }
+// }).then(function(data) {
+//   console.log(data);
+// });
